6a469f4694046fed7ef36ca2c4b14c4a06e60052
j <test>
2019-07-28 13:55:46 -0400

    getting chat working

--- index.js
+++ index.js
@@ -1,39 +1,18 @@
-
-const path = require('path');
-const express = require('express');
-
-const app = express();
-
-var http = require('http').createServer(app);
+var app = require('express')();
+var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
 
-//Static middleware
-app.use(express.static(path.join(__dirname)));
-app.use('/scripts',express.static(path.join(__dirname,'node_modules')));
-
-//Parsing middleware
-app.use('/', express.json());
-app.use(express.urlencoded({extended: true}));
-
-
-//API middleware
-//app.use('/api', require('./routes/api'));
-
-
-//Routes
-app.get('/', (req, res, next) => res.sendFile('index.html'));
-// app.get('/app.js', (req, res, next) =>
-//   res.sendFile(path.join(__dirname, '..', 'dist', 'main.js'))
-// );
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
 
 io.on('connection', function(socket){
-    socket.on('chat message', function(msg){
-      io.emit('chat message', msg);
-    });
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
   });
-  
+});
 
-module.exports = app;
-
-const PORT = process.env.PORT || 1337;
-app.listen(PORT, () => console.log(`listening on http://localhost:${PORT}`));
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
