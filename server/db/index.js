e4c7c37be0e3c73213680b2183d368175af3715e
Kyle Jiang <keeler@gmail.com>
2019-07-28 18:14:51 -0400

    work on this steve

--- server/db/index.js
+++ server/db/index.js
@@ -1,4 +1,19 @@
-const db = require('./connection.js')
-const Message = './models/Message.js'
+const db = require("./connection.js");
+const { Message, Chatuser, Pair, User, Chat } = require("./models");
+const {
+  fakeMessages,
+  fakeChats,
+  fakeChatusers,
+  fakePairs,
+  fakeUser
+} = require("./seeds");
 
 //define models relations here
+
+const syncAndSeed = () => {
+  return db.sync({ force: true }).then(() => {
+    fakeMessages.map(fakeMessage => {
+      Message.create({ text: fakeMessage.text });
+    });
+  });
+};
