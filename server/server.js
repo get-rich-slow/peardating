6819b8372415a837ed583258c753ac55e83c043c
mrderive@gmail.com <mrderive@gmail.com>
2019-08-04 15:43:40 -0400

    syncAndSeed button

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const { syncAndSeed } = require("./db");
 const app = require("./app");
 const PORT = process.env.PORT || 1337;
-
+/*
 syncAndSeed()
   .then(() => {
     app.listen(PORT, () =>
@@ -9,3 +9,21 @@ syncAndSeed()
     );
   })
   .catch(error => console.log(error));
+*/
+const server = 
+app
+  .listen(PORT, () => {
+    console.log(`listening on http://localhost:${PORT}`);
+  });
+
+const io = require("socket.io")(server);
+
+io.on("connection", (socket) => {
+  socket.on("syncAndSeed", (msg) => {
+    syncAndSeed()
+      .then(() => {
+        io.emit("syncAndSeed done");
+      })
+      .catch(error => console.log(error));
+  });
+});
